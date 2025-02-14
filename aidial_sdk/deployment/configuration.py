9d4ca63b90fa82ca5775ff52f10983e6fddd5513
Sergei Zinchenko <sergei.zinchenko@epam.com>
2025-02-14 16:28:58 +0100

    config with custom fields

--- aidial_sdk/deployment/configuration.py
+++ aidial_sdk/deployment/configuration.py
@@ -2,13 +2,14 @@ import fastapi
 
 from aidial_sdk.deployment.from_request_mixin import FromRequestDeploymentMixin
 from aidial_sdk.pydantic_v1 import BaseModel
+from aidial_sdk.utils.pydantic import ExtraForbidModel
+from typing import Optional, Dict, Any
 
+class ConfigurationRequestCustomFields(ExtraForbidModel):
+    application_properties: Optional[Dict[str, Any]] = None
 
 class ConfigurationRequest(FromRequestDeploymentMixin):
-    @staticmethod
-    async def get_request_body(request: fastapi.Request) -> dict:
-        return {}
-
+    custom_fields: Optional[ConfigurationRequestCustomFields] = None
 
 class ConfigurationResponse(BaseModel):
     class Config:
